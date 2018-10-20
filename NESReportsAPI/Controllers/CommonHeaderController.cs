8ff01f4a2a1347f0c27b487fbea08605592f9c15
gousela143 <32873671+gousela143@users.noreply.github.com>
2018-10-20 19:02:19 +0530

    common header API
    commited

--- NESReportsAPI/Controllers/CommonHeaderController.cs
+++ NESReportsAPI/Controllers/CommonHeaderController.cs
@@ -12,127 +12,153 @@ namespace NESReportsAPI.Controllers
     [RoutePrefix("api/common")]
     public class CommonHeaderController : ApiController
     {
-
         CommonHeaderBLL commonHeaderBLL = new CommonHeaderBLL();
+
         /// <summary>
-        /// 
+        /// Get States
         /// </summary>
         /// <returns></returns>
         [Route("states")]
-        public List<States> GetStates()
+        public IHttpActionResult GetStates()
         {
             try
-            { 
-                return commonHeaderBLL.GetStates();
+            {
+                return Ok(commonHeaderBLL.GetStates());
             }
             catch (Exception ex)
             {
-                return null;
+                return InternalServerError(ex);
             }
         }
 
         /// <summary>
-        /// 
+        /// Get Districts
         /// </summary>
         /// <returns></returns>
         [Route("districts/{stateIds}")]
-        public List<Districts> GetDistrict(string stateIds)
+        public IHttpActionResult GetDistrict(string stateIds)
         {
             try
             {
                 CommonHeaderBLL commonHeaderBLL = new CommonHeaderBLL();
-                return commonHeaderBLL.GetDistrict(stateIds);
+                return Ok(commonHeaderBLL.GetDistrict(stateIds));
             }
             catch (Exception ex)
             {
-                return null;
+                return InternalServerError(ex);
             }
         }
-
-        [Route("branches/{districtIds}")]
-        public List<Branches> GetBranches(string districtIds)
-        {
-            try
-            {
-                CommonHeaderBLL commonHeaderBLL = new CommonHeaderBLL();
-                return commonHeaderBLL.GetBranches(districtIds);
-            }
-            catch (Exception ex)
-            {
-                return null;
-            }
-        }
-
-        [Route("courses")]
-        public List<Course> GetClasses()
-        {
-            try
-            {
-                CommonHeaderBLL commonHeaderBLL = new CommonHeaderBLL();
-                return commonHeaderBLL.GetClasses();
-            }
-            catch (Exception ex)
-            {
-                return null;
-            }
-        }
-
 
         /// <summary>
-        /// 
+        /// Get Branchs
+        /// </summary>
+        /// <param name="districtIds"></param>
+        /// <returns></returns>
+        [Route("branches/{districtIds}")]
+        public IHttpActionResult GetBranches(string districtIds)
+        {
+            try
+            {
+                CommonHeaderBLL commonHeaderBLL = new CommonHeaderBLL();
+                return Ok(commonHeaderBLL.GetBranches(districtIds));
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        /// <summary>
+        /// Get Course
+        /// </summary>
+        /// <returns></returns>
+        [Route("courses")]
+        public IHttpActionResult GetClasses()
+        {
+            try
+            {
+                CommonHeaderBLL commonHeaderBLL = new CommonHeaderBLL();
+                return Ok(commonHeaderBLL.GetClasses());
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        /// <summary>
+        /// Get Subjects
+        /// </summary>
+        /// <returns></returns>
+        [Route("subjects")]
+        public IHttpActionResult GetSubjects()
+        {
+            try
+            {
+                CommonHeaderBLL commonHeaderBLL = new CommonHeaderBLL();
+                return Ok(commonHeaderBLL.GetSubjects());
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        /// <summary>
+        /// Get Category By ReportId
         /// </summary>
         /// <param name="reportType"></param>
         /// <returns></returns>
         [Route("categorybyreport/{reportType}")]
-        public AVReportDTO GetCategoryByReportId(int reportType)
+        public IHttpActionResult GetCategoryByReportId(int reportType)
         {
             try
             {
                 CommonHeaderBLL commonHeaderBLL = new CommonHeaderBLL();
-                return commonHeaderBLL.GetCategoryByReportId(reportType);
+                return Ok(commonHeaderBLL.GetCategoryByReportId(reportType));
             }
             catch (Exception ex)
             {
-                return null;
+                return InternalServerError(ex);
             }
         }
 
         /// <summary>
-        /// 
+        /// Get Sub-Category By Report Type and Category
         /// </summary>
         /// <param name="reportType"></param>
         /// <param name="category"></param>
         /// <returns></returns>
         [Route("subcategorybyreportandcategory/{reportType}/{category}")]
-        public AVReportDTO GetSubCategoryByReportTypeAndCategory(int reportType, int category)
+        public IHttpActionResult GetSubCategoryByReportTypeAndCategory(int reportType, int category)
         {
             try
             {
                 CommonHeaderBLL commonHeaderBLL = new CommonHeaderBLL();
-                return commonHeaderBLL.GetSubCategoryByReportTypeAndCategory(reportType, category);
+                return Ok(commonHeaderBLL.GetSubCategoryByReportTypeAndCategory(reportType, category));
             }
             catch (Exception ex)
             {
-                return null;
+                return InternalServerError(ex);
             }
         }
 
         /// <summary>
-        /// 
+        /// Get Reports 
         /// </summary>
         /// <param name="ReportType"></param>
         /// <returns></returns>
         [Route("reportcategory")]
-        public AVReportDTO GetReports()
+        public IHttpActionResult GetReports()
         {
             try
             {
                 CommonHeaderBLL commonHeaderBLL = new CommonHeaderBLL();
-                return commonHeaderBLL.GetReports();
+                return Ok(commonHeaderBLL.GetReports());
             }
             catch (Exception ex)
             {
-                return null;
+                return InternalServerError(ex);
             }
         }
     }
